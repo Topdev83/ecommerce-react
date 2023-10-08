de35c81291d0cde56929a7e4fff9a2cc00d29cfb
mykola927 <top.dev88@outlook.com>
2023-10-08 06:00:31 +0200

    updated:

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
-const User = require('../models/user');
-const jwt = require('jsonwebtoken'); // to generate signed token
-const expressJwt = require('express-jwt'); // for auth check
-const { errorHandler } = require('../helpers/dbErrorHandler');
+/** @format */
 
-require('dotenv').config();
+const User = require("../models/user");
+const jwt = require("jsonwebtoken"); // to generate signed token
+const expressJwt = require("express-jwt"); // for auth check
+const { errorHandler } = require("../helpers/dbErrorHandler");
+
+require("dotenv").config();
 
 exports.signup = (req, res) => {
-  // console.log('req.body', req.body);
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
@@ -39,12 +40,9 @@ exports.signin = (req, res) => {
       });
     }
     // generate a signed token with user id and secret
-    const token = jwt.sign(
-      { _id: user._id },
-      process.env.JWT_SECRET
-    );
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
     // persist the token as 't' in cookie with expiry date
-    res.cookie('t', token, { expire: new Date() + 9999 });
+    res.cookie("t", token, { expire: new Date() + 9999 });
     // return response with user and token to frontend client
     const { _id, name, email, role } = user;
     return res.json({ token, user: { _id, email, name, role } });
@@ -52,21 +50,21 @@ exports.signin = (req, res) => {
 };
 
 exports.signout = (req, res) => {
-  res.clearCookie('t');
-  res.json({ message: 'Signout success' });
+  res.clearCookie("t");
+  res.json({ message: "Signout success" });
 };
 
 exports.requireSignin = expressJwt({
   secret: process.env.JWT_SECRET,
   // algorithms: ['RS256'],
-  userProperty: 'auth',
+  userProperty: "auth",
 });
 
 exports.isAuth = (req, res, next) => {
   let user = req.profile && req.auth && req.profile._id == req.auth._id;
   if (!user) {
     return res.status(403).json({
-      error: 'Access denied',
+      error: "Access denied",
     });
   }
   next();
@@ -75,7 +73,7 @@ exports.isAuth = (req, res, next) => {
 exports.isAdmin = (req, res, next) => {
   if (req.profile.role === 0) {
     return res.status(403).json({
-      error: 'Admin resource! Access denied',
+      error: "Admin resource! Access denied",
     });
   }
   next();
--- reset.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch -f --env-filter "
-    GIT_AUTHOR_NAME='mykola927'
-    GIT_AUTHOR_EMAIL='top.dev88@outlook.com'
-    GIT_COMMITTER_NAME='mykola927'
-    GIT_COMMITTER_EMAIL='top.dev88@outlook.com'
-  " HEAD
